a5ef4269b6384c5e4ec7307dceb4f26290fdb02e
Ahmed Abdelalem <Ahmed.Abdelalem60@gmail.com>
2022-03-10 22:45:39 +0200

    sparse array and matrix

---
+++ section 6 : project sparse array and matrix/06 project 1 - sparse matrix/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+ArrayLinkedList array2(10);
+	// array2.set_value(1, 4);
+	// array2.set_value(3, 7);
+	// array2.set_value(4, 6);
+
+	// array.add(array2);
+	// array.print_array();
+	// // 0 0 20 0 41 50 4 73 0 0
